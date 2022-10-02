ba4c43ba7da2369343d8977915443f4683a9a38c
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2022-10-02 21:15:34 +0300

    updates

--- api/routes/documents.js
+++ api/routes/documents.js
@@ -29,41 +29,6 @@ conn.once('open', () => {
     gfs.collection('reportFiless')
     // gfs = Grid(conn.db)
     // gfs.collection('reportFiless')
-
-    router.get('/files/:id', (req, res) => {
-        const id = req.params.id
-        if (!id || id === 'undefined')
-            return res.status(400).send('no document found')
-        const _id = new mongoose.Types.ObjectId(id)
-        gfs.files.find({ _id }).toArray((err, files) => {
-            console.log('files', files)
-            if (!files || files.length === 0)
-                return res.status(400).send('no files exists')
-
-            let data = []
-            // let readstream = gridfsBucket.createReadStream({
-            //     filename: files[0].filename,
-            // })
-
-            let readstream = gridfsBucket.openDownloadStream(_id)
-            console.log('readStream', readstream)
-            readstream.on('data', function (chunk) {
-                data.push(chunk)
-            })
-
-            readstream.on('end', function () {
-                data = Buffer.concat(data)
-                let fileOutput =
-                    `data:${files[0].contentType};base64,` +
-                    Buffer(data).toString('base64')
-                res.end(fileOutput)
-            })
-
-            // res.set('Content-Type', files[0].contentType)
-            // return readstream.pipe(res)
-            //gridfsBucket.openDownloadStream(_id).pipe(res)
-        })
-    })
 })
 
 const storage = new GridFsStorage({
@@ -120,5 +85,40 @@ const uploadMiddleware = (req, res, next) => {
         next()
     })
 }
+
+router.get('/files/:id', (req, res) => {
+    const id = req.params.id
+    if (!id || id === 'undefined')
+        return res.status(400).send('no document found')
+    const _id = new mongoose.Types.ObjectId(id)
+    gfs.files.find({ _id }).toArray((err, files) => {
+        console.log('files', files)
+        if (!files || files.length === 0)
+            return res.status(400).send('no files exists')
+
+        let data = []
+        // let readstream = gridfsBucket.createReadStream({
+        //     filename: files[0].filename,
+        // })
+
+        let readstream = gridfsBucket.openDownloadStream(_id)
+        console.log('readStream', readstream)
+        readstream.on('data', function (chunk) {
+            data.push(chunk)
+        })
+
+        readstream.on('end', function () {
+            data = Buffer.concat(data)
+            let fileOutput =
+                `data:${files[0].contentType};base64,` +
+                Buffer(data).toString('base64')
+            res.end(fileOutput)
+        })
+
+        // res.set('Content-Type', files[0].contentType)
+        // return readstream.pipe(res)
+        //gridfsBucket.openDownloadStream(_id).pipe(res)
+    })
+})
 
 module.exports = router
